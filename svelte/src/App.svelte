8d6f2ef0d4988272121ff41c88e2449c0c48b125
Ben Giacalone <bensgiacalone@gmail.com>
2023-10-31 17:40:19 -0400

    Baseline commit

---
+++ svelte/src/App.svelte
@@ -0,0 +1,16 @@
+<script>
+	export let bindings;
+</script>
+
+<main>
+	<p>{bindings.add(6, 7)}</p>
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+</style>
